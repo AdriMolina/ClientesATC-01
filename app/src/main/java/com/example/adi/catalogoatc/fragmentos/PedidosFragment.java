3de97ef3cd784fb1f88f68ba41591abb97276b72
AdriiMolina <ti13110051@itess.edu.mx>
2018-01-22 15:53:47 -0600

    Pedidos

--- app/src/main/java/com/example/adi/catalogoatc/fragmentos/PedidosFragment.java
+++ app/src/main/java/com/example/adi/catalogoatc/fragmentos/PedidosFragment.java
@@ -1,20 +1,42 @@
 package com.example.adi.catalogoatc.fragmentos;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.adi.catalogoatc.ModeloLista.modeloCatalogo;
 import com.example.adi.catalogoatc.R;
+import com.example.adi.catalogoatc.Recursos.Basic;
+import com.example.adi.catalogoatc.adapters.CatalogoAdapter;
+import com.example.adi.catalogoatc.adapters.HistorialAdapter;
+import com.example.adi.catalogoatc.ModeloLista.modeloHistorial;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
-public class PedidosFragment extends Fragment {
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class PedidosFragment extends Fragment implements Basic, Response.Listener<JSONArray>, Response.ErrorListener {
+    private ProgressDialog progressDialog;
+    String url;
+    private ListView listView;
+    View view;
+    HistorialAdapter adapter;
 
     private OnFragmentInteractionListener mListener;
 
@@ -26,8 +48,6 @@ public class PedidosFragment extends Fragment {
     public static PedidosFragment newInstance(String param1, String param2) {
         PedidosFragment fragment = new PedidosFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -43,8 +63,46 @@ public class PedidosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+         view = inflater.inflate(R.layout.fragment_pedidos, container, false);
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pedidos, container, false);
+
+        listView = (ListView)view.findViewById(R.id.listaPedidos);
+
+        //Coloca el dialogo de carga
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setTitle("En Proceso");
+        progressDialog.setMessage("Un momento...");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.show();
+
+        //Inicia la peticion
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        String consulta = "SELECT o.id, o.id, o.folio,  DATE(o.fecha)," +
+                            " (SELECT SUM( od.precio_final * od.cantidad)" +
+                            " FROM orden_descripcion od, orden ord" +
+                            " where od.orden_id = ord.id" +
+                            " AND ord.id = o.id) AS total" +
+                            " from orden o" +
+                            " where o.id not in(Select orden_id from orden_completa)" +
+                            " and o.cliente_id =" +IDUsusario+
+                            " order by o.fecha desc;";
+
+        consulta = consulta.replace(" ", "%20");
+        String cadena = "?host=" + HOST + "&db=" + DB + "&usuario=" + USER + "&pass=" + PASS + "&consulta=" + consulta;
+        url= SERVER + RUTA + "consultaGeneral.php" + cadena;
+        Log.i("info", url);
+
+        //Hace la petición String
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, this, this);
+
+        //Agrega y ejecuta la cola
+        queue.add(request);
+
+
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -54,7 +112,21 @@ public class PedidosFragment extends Fragment {
         }
     }
 
-    
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        progressDialog.hide();
+        Log.i("mensaje", error.toString());
+        Toast.makeText(getContext(), "Error en el WebService", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), url, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onResponse(JSONArray response) {
+        progressDialog.hide();
+        adapter = new HistorialAdapter(getContext(), modeloHistorial.sacarListaClientes(response));
+        listView.setAdapter(adapter);
+    }
+
 
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name

5e4beff31f55541c514a764edb7e085ffc7558d1
AdriiMolina <ti13110051@itess.edu.mx>
2018-02-15 17:38:27 -0600

    Posicion en sidney

--- app/src/main/java/com/example/adi/catalogoatc/clases/MapsLocalizacionActivity.java
+++ app/src/main/java/com/example/adi/catalogoatc/clases/MapsLocalizacionActivity.java
@@ -1,5 +1,13 @@
 package com.example.adi.catalogoatc.clases;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -14,6 +22,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsLocalizacionActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private double latitud;
+    private double longitud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +37,76 @@ public class MapsLocalizacionActivity extends FragmentActivity implements OnMapR
         mapFragment.getMapAsync(this);
     }
 
+    //Metodo que verifica si tenemos permiso de gps en el telefono celular
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch ((requestCode)) {
+            case 10:
+                geolocalizar();
+                break;
+            default:
+                break;
+        }
+    }
+
+    //vrifica si tiene permisos
+    public void geolocalizar() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.INTERNET}, 10);
+            }
+            return;
+        }
+
+        locationManager.requestLocationUpdates("gps", 0, 0, locationListener);
+    }
+
+
+    public void setLatLong (double dlatitud, double dlongitud){
+        this.latitud = dlatitud;
+        this.longitud = dlongitud;
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        locationListener = (LocationListener) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitud = location.getLatitude();
+                longitud = location.getLongitude();
+
+                //setLatLong(location.getLatitude(), location.getLatitude());
+
+                LatLng actual = new LatLng(latitud, longitud);
+                mMap.clear();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        //Poner los con troles de zoom en el mapa
+        mMap.getUiSettings().setZoomControlsEnabled(true);
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
